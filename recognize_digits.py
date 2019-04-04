50196240cc0f3c9f1b1beb855ee08e2a201b1c79
seungyeon <ream150346@gmail.com>
2019-04-04 19:58:44 +0900

    first commit

---
+++ recognize_digits.py
@@ -0,0 +1,137 @@
+#USAGE
+#python recognize_digits.py
+
+#import the necessary packages
+from imutils.perspective import four_point_transform
+from imutils import contours
+import imutils
+import cv2
+
+DIGITS_LOOKUP = {
+	(1, 1, 1, 0, 1, 1, 1): 0,
+	(0, 0, 1, 0, 0, 1, 0): 1,
+	(1, 0, 1, 1, 1, 1, 0): 2,
+	(1, 0, 1, 1, 0, 1, 1): 3,
+	(0, 1, 1, 1, 0, 1, 0): 4,
+	(1, 1, 0, 1, 0, 1, 1): 5,
+	(1, 1, 0, 1, 1, 1, 1): 6,
+	(1, 0, 1, 0, 0, 1, 0): 7,
+	(1, 1, 1, 1, 1, 1, 1): 8,
+	(1, 1, 1, 1, 0, 1, 1): 9
+}
+
+#이미지 읽기
+image = cv2.imread("example.jpg")
+#사진의 크기를 조정한다.
+image = imutils.resize(image, height=500)
+#grayscale과정
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#가우시안 블러를 사용하는 이유는 밝기가 차이가 있을때 같은 뿌염으로 한다.그리고 애매한 엣지들을 
+#안찾고 edge를 찾을 수 있게 한다.
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(blurred, 50, 200, 255)
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+#정렬을 contourArea에 따라서 내림차순 정렬한다.
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
+displayCnt = None
+
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	#4개의 점을 뽑기 위해 둥근 모서리의 점을 추측한다.
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	#닫힌 윤곽에서 찾기
+
+	# if the contour has four vertices, then we have found
+	# the thermostat display
+	if len(approx) == 4:
+		displayCnt = approx
+		break
+
+# extract the thermostat display, apply a perspective transform
+# to it
+warped = four_point_transform(gray, displayCnt.reshape(4, 2))
+output = four_point_transform(image, displayCnt.reshape(4, 2))
+#4.2는 4개의 점+ (x,y)
+
+# threshold the warped image, then apply a series of morphological
+# operations to cleanup the thresholded image
+thresh = cv2.threshold(warped, 0, 255,
+	cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+
+# find contours in the thresholded image, then initialize the
+# digit contours lists
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+digitCnts = []
+
+# loop over the digit area candidates
+for c in cnts:
+	# compute the bounding box of the contour
+	(x, y, w, h) = cv2.boundingRect(c)
+
+	# if the contour is sufficiently large, it must be a digit
+	if w >= 15 and (h >= 30 and h <= 40):
+		digitCnts.append(c)
+
+# sort the contours from left-to-right, then initialize the
+# actual digits themselves
+digitCnts = contours.sort_contours(digitCnts,
+	method="left-to-right")[0]
+digits = []
+
+# loop over each of the digits
+for c in digitCnts:
+	# extract the digit ROI
+	(x, y, w, h) = cv2.boundingRect(c)
+	roi = thresh[y:y + h, x:x + w]
+
+	# compute the width and height of each of the 7 segments
+	# we are going to examine
+	(roiH, roiW) = roi.shape
+	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+	dHC = int(roiH * 0.05)
+
+	# define the set of 7 segments
+	segments = [
+		((0, 0), (w, dH)),	# top
+		((0, 0), (dW, h // 2)),	# top-left
+		((w - dW, 0), (w, h // 2)),	# top-right
+		((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+		((0, h // 2), (dW, h)),	# bottom-left
+		((w - dW, h // 2), (w, h)),	# bottom-right
+		((0, h - dH), (w, h))	# bottom
+	]
+	on = [0] * len(segments)
+
+	# loop over the segments
+	for (i, ((xA, yA), (xB, yB))) in enumerate(segments):
+		# extract the segment ROI, count the total number of
+		# thresholded pixels in the segment, and then compute
+		# the area of the segment
+		segROI = roi[yA:yB, xA:xB]
+		total = cv2.countNonZero(segROI)
+		area = (xB - xA) * (yB - yA)
+
+		# if the total number of non-zero pixels is greater than
+		# 50% of the area, mark the segment as "on"
+		if total / float(area) > 0.5:
+			on[i]= 1
+
+	# lookup the digit and draw it on the image
+	digit = DIGITS_LOOKUP[tuple(on)]
+	digits.append(digit)
+	cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 1)
+	cv2.putText(output, str(digit), (x - 10, y - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 255, 0), 2)
+
+# display the digits
+print(u"{}{}.{} \u00b0C".format(*digits))
+cv2.imshow("Input", image)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
